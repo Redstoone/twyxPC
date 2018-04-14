450b8a9fb57fb750b9ad937f846fb6a109c3fdf0
redstoone <cy_jing_2010@163.com>
2018-04-14 23:13:37 +0800

    修改

--- assets/js/course.js
+++ assets/js/course.js
@@ -1,57 +1,4 @@
 $(function() {
-  // var special =  [
-  //   {
-  //     id: 1,
-  //     title: '声乐歌舞',
-  //     tips: '4-8周岁儿童',
-  //     class_time: '150分钟/16周次/一学期每次150分钟',
-  //     point: ' 发挥儿童歌舞表演天性，全面塑造舞台艺术表现力',
-  //     bgc: 'c1'
-  //   }, {
-  //     id: 2,
-  //     title: '影视表演',
-  //     tips: '4-8周岁儿童',
-  //     class_time: '每学期16周次，每次150分钟',
-  //     point: ' 学在教室，用在生活，培养观察和理解力，提高表现能力与沟通能力',
-  //     bgc: 'c2'
-  //   }, {
-  //     id: 3,
-  //     title: '民族舞',
-  //     tips: '4-8周岁儿童',
-  //     class_time: '每学期16周次，每次150分钟',
-  //     point: '学习民族舞精髓，体验民族舞风情 ',
-  //     bgc: 'c3'
-  //   }, {
-  //     id: 4,
-  //     title: '涂鸦创想课',
-  //     tips: '4-8周岁儿童',
-  //     class_time: '滚动开班，每周一次，每次90分钟',
-  //     point: '培养美术兴趣，玩与学结合，发掘孩子艺术天赋 ',
-  //     bgc: 'c4'
-  //   }, {
-  //     id: 5,
-  //     title: '素描课',
-  //     tips: '9岁以上零基础孩子',
-  //     class_time: '滚动开班，每周一次，一次150分钟 ',
-  //     point: '感受艺术的美感，培养孩子的观察力、造型能力与表现能力 ',
-  //     bgc: 'c5'
-  //   }, {
-  //     id: 6,
-  //     title: '少儿合唱团',
-  //     tips: '4-8周岁儿童',
-  //     class_time: '每学期16周次，每次150分钟',
-  //     point: '开发儿童智力，培养专注力与团队合作能力',
-  //     bgc: 'c6'
-  //   }, {
-  //     id: 7,
-  //     title: '钢琴课',
-  //     tips: '4岁以上儿童',
-  //     class_time: '一对一教学，每周一次，每次45分钟',
-  //     point: '专业院校师资，感受古典音乐的魅力，打造孩子的贵族气质 ',
-  //     bgc: 'c7'
-  //   }
-  // ];
-
   var _content = ''
   $.ajax({
     type: "GET",
@@ -86,13 +33,6 @@ $(function() {
               '</div>';
           }
         })
-        _content += '<div class="item c-item c-item-more" style="opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0);">' +
-          '<p class="waiting">等待开设</p>'+
-          '<p class="course-more">播音主持班</p>'+
-          '<p class="course-more">芭蕾舞班</p>'+
-          '<p class="course-more">拉丁舞班</p>'+
-          '<p class="course-more">爵士舞班</p>'+
-          '</div>';
         $('.course-list').empty().append(_content)
       }
     }
---
+++ assets/js/detail.js
@@ -0,0 +1,98 @@
+$(function () {
+  var id = null;
+  var category = null
+
+  var _querystring = parseQueryString(window.location.href)
+
+  console.log(_querystring)
+
+  if (_querystring.c === 'news') {
+    getNewsDetail(_querystring.id)
+  } else if (_querystring.c === 'growup') {
+    getGrowupDetail(_querystring.id)
+  } else if (_querystring.c === 'open'){
+    getOpenDetail(_querystring.id)
+  }
+
+
+  function getNewsDetail(id) {
+    $.ajax({
+      type: "GET",
+      url: "http://api.twyxedu.com/api/news/detail",
+      data: {
+        newsId: id
+      },
+      success: function (res) {
+        if (res.status === 'succ') {
+          $('.dw-title').text(res.data.name)
+          $('.dw-time').text(res.data.createTime)
+          document.querySelector('.dw-cont').innerHTML = res.data.content
+          if (res.data.videoUrl) {
+            document.querySelector('.video-box').innerHTML = res.data.videoUrl
+          }
+        }
+      }
+    })
+  }
+
+  function getGrowupDetail(id) {
+    $.ajax({
+      type: "GET",
+      url: "http://api.twyxedu.com/api/activity/detail",
+      data: {
+        activityId: id
+      },
+      success: function (res) {
+        if (res.status === 'succ') {
+          $('.dw-title').text(res.data.name)
+          $('.dw-time').text(res.data.time)
+          document.querySelector('.dw-cont').innerHTML = res.data.content
+          if (res.data.videoUrl) {
+            document.querySelector('.video-box').innerHTML = res.data.videoUrl
+          }
+        }
+      }
+    })
+  }
+
+  function getOpenDetail(id) {
+    $.ajax({
+      type: "GET",
+      url: "http://api.twyxedu.com/api/show/clazz/detail",
+      data: {
+        showId: id
+      },
+      success: function (res) {
+        if (res.status === 'succ') {
+          $('.dw-title').text(res.data.name)
+          $('.dw-time').text(res.data.time)
+          document.querySelector('.dw-cont').innerHTML = res.data.content
+          if (res.data.videoUrl) {
+            document.querySelector('.video-box').innerHTML = res.data.videoUrl
+          }
+        }
+      }
+    })
+  }
+
+  function parseQueryString(url) {
+    var arr = []
+    var res = {}
+    url = url.split('#')[0]
+    arr = url.split('?')
+    arr.shift()
+    var queryStr = arr.join('?')
+    if (queryStr.trim().length === 0) {
+      return res
+    }
+
+    arr = queryStr.split('&')
+    for (var i = 0; i < arr.length; i++) {
+      var itemArr = arr[i].split('=')
+      var name = itemArr.shift()
+      var value = itemArr.join('=')
+      res[name] = value
+    }
+    return res
+  }
+})
