a1ccac40d4c321233fd15e925cb4c23ec0032cf9
redstoone <cy_jing_2010@163.com>
2018-04-21 00:58:28 +0800

    修改

--- assets/js/course.js
+++ assets/js/course.js
@@ -9,12 +9,13 @@ $(function() {
         res.data.array.forEach(function (item, index) {
           var _time = parseInt(item.zc) > 0 ? '每学期' + item.zc + '周次' : item.zc;
           _time += ', 每次' + item.ksl +'课时，共'+ item.sc +'分钟';
+          _viewDtail = item.imgUrl ? '<span class="viewDetail" data-url="' + item.imgUrl + '">查看详情</span>'  : ''
           if (index % 5 == 1) {
             _content += '<div class="item c-item" style="opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0);">' +
               '  <div class="names">' + item.name + '</div>' +
               '  <div class="info">' +
               '    <span>' + item.age + ' | ' + _time + '</span>' +
-              '    <span>' + item.intro + '</span>' +
+              '    <span>' + item.intro + '</span>' + _viewDtail +
               '  </div>' +
               '  <div class="thumb thumb1" style="background-image: url(' + item.coverUrl + ');">' +
               '    <div class="thumb-child" style="opacity: 1;"></div>' +
@@ -28,7 +29,7 @@ $(function() {
               '  <div class="names">' + item.name + '</div>' +
               '  <div class="info">' +
               '    <span>' + item.age + ' | ' + _time + '</span>' +
-              '    <span>' + item.intro + '</span>' +
+              '    <span>' + item.intro + '</span>' + _viewDtail +
               '  </div>' +
               '</div>';
           }
@@ -37,4 +38,12 @@ $(function() {
       }
     }
   });
+
+  $('.course-list').on('click', '.viewDetail', function () {
+    $('.guzhu-info-more').addClass('open').find('.gi-content').empty().append('<img src="' + $(this).attr('data-url')+'">')
+  })
+
+  $('.guzhu-info-more').on('click', '#J_guzhuinfo_close', function () {
+    $('.guzhu-info-more').removeClass('open')
+  })
 })
